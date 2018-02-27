db6f432bfeb96527b3c4e7d26f25ae654a0cb05f
AJIajithmech <36884000+AJIajithmech@users.noreply.github.com>
2018-02-27 16:29:52 +0530

    Create aa.c

---
+++ aa.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	scanf("%d",&a);
+	if(a==0)
+	{
+		printf("it is zero");
+		
+	}
+	else if(a<0)
+	{
+		printf("it is negative");
+		
+	}
+	else
+	{
+		printf("it is positive");
+		
+	}
+	return 0;
+}
