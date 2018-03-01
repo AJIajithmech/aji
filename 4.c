962159ca96b7d583dd586f4e85e12135c093e5eb
AJIajithmech <36884000+AJIajithmech@users.noreply.github.com>
2018-03-01 16:00:09 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char s;
+scanf("%s",&s);
+if(((s>='a') && (s<='z')) || ((s>='A') && (s<='Z')))
+{
+printf("\n the given is alphabet");
+}
+else
+{
+printf("\n it is not alphabet");
+}
+}
